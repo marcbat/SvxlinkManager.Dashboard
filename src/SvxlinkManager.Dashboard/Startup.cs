ce02d3c4caed716f15f61276479f1992efd2766e
Marc Battaglia <marcbat79@gmail.com>
2021-03-04 21:38:11 +0100

    Ajout de l'injection de dependence

--- src/SvxlinkManager.Dashboard/Startup.cs
+++ src/SvxlinkManager.Dashboard/Startup.cs
@@ -30,6 +30,7 @@ namespace SvxlinkManager.Dashboard
       services.AddRazorPages();
       services.AddServerSideBlazor();
       services.AddSingleton<WeatherForecastService>();
+      services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
