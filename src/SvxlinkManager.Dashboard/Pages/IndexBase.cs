d589a0d7561865faa7bf59eccc0b5e44313d2dee
Marc Battaglia <marcbat79@gmail.com>
2021-03-17 12:56:00 +0100

    Dérarrage automatique

--- src/SvxlinkManager.Dashboard/Pages/IndexBase.cs
+++ src/SvxlinkManager.Dashboard/Pages/IndexBase.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
 
 using SvxlinkManager.Common.Models;
 using SvxlinkManager.Common.Service;
@@ -11,26 +14,145 @@ using System.Threading.Tasks;
 
 namespace SvxlinkManager.Dashboard.Pages
 {
-  public class IndexBase : ComponentBase
+  public class IndexBase : ComponentBase, IDisposable
   {
-    public readonly string applicationPath = Directory.GetCurrentDirectory();
-
     protected override async Task OnInitializedAsync()
     {
       await base.OnInitializedAsync();
 
-      var channel = new ChannelBase { Name = "Salon Suisse Rommand" };
+      SvxLinkService.NodeConnected += SvxLinkService_NodeConnected;
 
-      SvxLinkService.StartSvxlink(channel, pidFile: "/var/run/svxlink.pid", runAs: "root", configFile: $"{applicationPath}/SvxlinkConfig/svxlink.conf");
+      SvxLinkService.NodeDisconnected += SvxLinkService_NodeDisconnected;
+
+      SvxLinkService.NodeTx += SvxLinkService_NodeTx;
+
+      SvxLinkService.NodeRx += SvxLinkService_NodeRx;
+
+      SvxLinkService.Error += SvxLinkService_Error;
     }
 
     [Inject]
     public SvxlinkServiceBase SvxLinkService { get; set; }
 
+    [Inject]
+    public ILogger<IndexBase> Logger { get; set; }
+
+    [Inject]
+    public TelemetryClient Telemetry { get; set; }
+
+    [Inject]
+    public IJSRuntime Js { get; set; }
+
+    public Common.Models.Node CurrentTxNode { get; set; }
+
     public List<Common.Models.Node> Nodes
     {
       get => SvxLinkService.Nodes.OrderBy(n => n.Name).ToList();
       set => SvxLinkService.Nodes = value;
+    }
+
+    /// <summary>Show new toast.</summary>
+    /// <param name="title">Toast title</param>
+    /// <param name="body">Toast body</param>
+    /// <param name="type">Toast type. Accept : success, info, danger</param>
+    /// <param name="autohide">Autohide, always false if type is danger</param>
+    /// <param name="delay">Autohide delay in second</param>
+    private async Task ShowToastAsync(string title, string body, string type, bool autohide = true, int delay = 5000) =>
+      await Js.InvokeVoidAsync("addToast", Guid.NewGuid().ToString(), title, body, type, DateTime.Now.ToString("HH:mm:ss"), autohide, delay);
+
+    protected async Task ShowInfoToastAsync(string title, string body, bool autohide = true, int delay = 5000) =>
+       await ShowToastAsync(title, body, "info", autohide, delay);
+
+    protected async Task ShowErrorToastAsync(string title, string body, bool autohide = true, int delay = 10000) =>
+      await ShowToastAsync(title, body, "danger", autohide, delay);
+
+    private async void SvxLinkService_NodeConnected(Common.Models.Node n)
+    {
+      try
+      {
+        await InvokeAsync(() => StateHasChanged());
+        await ShowInfoToastAsync(n.Name, "A rejoint le salon.");
+      }
+      catch (Exception e)
+      {
+        Logger.LogError($"Impossible d'indiquer qu'un noeud a rejoint le salon. {e.Message}");
+        Telemetry.TrackException(e);
+      }
+    }
+
+    private async void SvxLinkService_NodeDisconnected(Common.Models.Node n)
+    {
+      try
+      {
+        await InvokeAsync(() => StateHasChanged());
+        await ShowInfoToastAsync(n.Name, "A quitté le salon.");
+      }
+      catch (Exception e)
+      {
+        Logger.LogError($"Impossible d'indiquer qu'un noeus a quitté le salon. {e.Message}");
+        Telemetry.TrackException(e);
+      }
+    }
+
+    private void SvxLinkService_NodeTx(Common.Models.Node n)
+    {
+      try
+      {
+        CurrentTxNode = n;
+        InvokeAsync(() => StateHasChanged());
+      }
+      catch (Exception e)
+      {
+        Logger.LogError($"Impossible de passer le node en TX. {e.Message}");
+        Telemetry.TrackException(e);
+      }
+    }
+
+    private void SvxLinkService_NodeRx(Common.Models.Node n)
+    {
+      try
+      {
+        CurrentTxNode = null;
+        InvokeAsync(() => StateHasChanged());
+      }
+      catch (Exception e)
+      {
+        Logger.LogError($"Impossible de repasser le node en RX. {e.Message}");
+        Telemetry.TrackException(e);
+      }
+    }
+
+    private async void SvxLinkService_Error(string t, string b)
+    {
+      try
+      {
+        await ShowErrorToastAsync(t, b);
+      }
+      catch (Exception e)
+      {
+        Logger.LogError($"Impossible de mettre à jour d'afficher le toast. {e.Message}");
+        Telemetry.TrackException(e);
+      }
+    }
+
+    protected async override Task OnAfterRenderAsync(bool firstRender)
+    {
+      await base.OnAfterRenderAsync(firstRender).ConfigureAwait(false);
+
+      await Js.InvokeVoidAsync("SetPopOver");
+    }
+
+    public void Dispose()
+    {
+      SvxLinkService.NodeConnected -= SvxLinkService_NodeConnected;
+
+      SvxLinkService.NodeDisconnected -= SvxLinkService_NodeDisconnected;
+
+      SvxLinkService.NodeTx -= SvxLinkService_NodeTx;
+
+      SvxLinkService.NodeRx -= SvxLinkService_NodeRx;
+
+      SvxLinkService.Error -= SvxLinkService_Error;
     }
   }
 }
--- src/SvxlinkManager.Dashboard/Startup.cs
+++ src/SvxlinkManager.Dashboard/Startup.cs
@@ -5,11 +5,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using SvxlinkManager.Common.Models;
 using SvxlinkManager.Common.Service;
 using SvxlinkManager.Dashboard.Data;
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,6 +19,8 @@ namespace SvxlinkManager.Dashboard
 {
   public class Startup
   {
+    private readonly string StreamingPoliciyName = "Streaming";
+
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
@@ -28,6 +32,11 @@ namespace SvxlinkManager.Dashboard
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddCors(o =>
+      {
+        o.AddPolicy(StreamingPoliciyName, b => b.WithOrigins("http://salonsuisseromand.northeurope.cloudapp.azure.com:8000"));
+      });
+
       services.AddRazorPages();
       services.AddServerSideBlazor();
       services.AddSingleton<SvxlinkServiceBase>();
@@ -46,9 +55,23 @@ namespace SvxlinkManager.Dashboard
         app.UseExceptionHandler("/Error");
       }
 
+      using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+      {
+        var applicationPath = Directory.GetCurrentDirectory();
+        var channel = new ChannelBase { Name = "Salon Suisse Romand" };
+
+        var svxlinkservice = serviceScope.ServiceProvider.GetRequiredService<SvxlinkServiceBase>();
+        if (env.IsDevelopment())
+          svxlinkservice.StartSvxlink(channel, pidFile: "/var/run/svxlink.pid", runAs: "root", configFile: $"{applicationPath}/SvxlinkConfig/svxlink.conf");
+        else
+          svxlinkservice.StartSvxlink(channel, pidFile: "/var/run/svxlink.pid", runAs: "root");
+      }
+
       app.UseStaticFiles();
 
       app.UseRouting();
+
+      app.UseCors(StreamingPoliciyName);
 
       app.UseEndpoints(endpoints =>
       {
